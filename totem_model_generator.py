249fc7842ba3804b5fd5a6d4de8c1ae9579f4456
YehowahLiu <redme@live.cn>
2021-08-21 11:36:45 +0800

    使用Steve作为模板 添加批量生成功能

--- totem_model_generator.py
+++ totem_model_generator.py
@@ -2,7 +2,9 @@ import requests
 import base64
 import shutil
 import json
+import sys
 import os
+import re
 
 
 IMG_DIR = os.path.join('target', 'image', '')
@@ -17,10 +19,12 @@ def get_uuid(name: str) -> str:
     #     "name": "401Unauthorized",
     #     "id": "0c3d72f4eab54223bfe226cd8af4bd56"
     # }
-    print(f'Try to get the uuid of {name} from mojang...')
+    print(f'从 mojang api 获取玩家 {name} 的 uuid 中...')
     response = requests.get("https://api.mojang.com/users/profiles/minecraft/" + name)
+    if response.status_code != 200:
+        raise RuntimeError('无法获取 uuid!')
     data = response.json()
-    print(f'Done! The uuid is {data["id"]}')
+    print(f'成功! 获取到的 uuid 为 {data["id"]}')
     return data['id']
 
 
@@ -37,15 +41,17 @@ def get_skin_url(uuid: str) -> str:
     #     }
     #   ]
     # }
-    print(f'Try to get the skin url of {uuid} from mojang...')
+    print(f'获取 {uuid} 的皮肤地址中...')
     response = requests.get("https://sessionserver.mojang.com/session/minecraft/profile/"+uuid)
+    if response.status_code != 200:
+        raise RuntimeError("获取皮肤地址失败!")
     data = response.json()
     for item in data['properties']:
         if item['name'] != 'textures':
             continue
-        print('Done!')
+        print('完成!')
         return base64_decode(item['value'])
-    raise RuntimeError("Couldn't get the skin url!")
+    raise RuntimeError("获取皮肤地址失败!")
 
 
 def base64_decode(codes: str) -> str:
@@ -60,10 +66,10 @@ def base64_decode(codes: str) -> str:
     #     }
     #   }
     # }
-    print('Conventing base64 string into plain text...')
+    print('解码base64中...')
     url_data = json.loads(base64.b64decode(codes))
     url = url_data['textures']['SKIN']['url']
-    print(f'Done! The Convented URL is {url}')
+    print(f'解码完成! 解码后的皮肤地址为: {url}')
     return url
 
 
@@ -72,51 +78,70 @@ def save_img(name: str) -> str:
     url = get_skin_url(uuid)
     skin_data = requests.get(url)
     if not os.path.exists(IMG_DIR):
-        print('Clearing previous file...')
         os.makedirs(IMG_DIR)
     file_path = os.path.join(IMG_DIR, name + '.png')
     with open(file_path, 'ab') as f:
-        print('Writing skin image ...')
+        print('写入皮肤图片中 ...')
         f.write(skin_data.content)
     return file_path
 
 
 def update_meta(name, template_dir=TEMPLATE_DIR):
     meta_file = os.path.join(template_dir, 'pack.mcmeta')
-    print('Loading meta file ...')
+    print('加载元数据中...')
     meta = json.load(open(meta_file))
-    print('Modifying meta file ...')
-    meta['pack']['description'] = f'§e不死图腾手办 for §b{name}\n§3让不死图腾变成{name}的手办'
-    with open(meta_file, 'w') as f:
-        print('Saving meta file changes ...')
-        f.write(json.dumps(meta))
+    print('修改元数据中...')
+    meta['pack']['description'] = f'§e不死图腾手办 for §b{name}'
+    with open(meta_file, 'w', encoding='utf-8') as f:
+        print('保存变更后的元数据文件中...')
+        f.write(json.dumps(meta, indent=4, ensure_ascii=False))
 
 
 def pack(name: str, target_dir: str = RESOURCEPACK_DIR):
     target_image = os.path.join(TEMPLATE_DIR, 'assets', 'customtotem', 'textures', 'entity', 'playerskin.png')
     if os.path.exists(target_image):
-        print('Clearing previous build cache ...')
+        print('清理上次构建...')
         os.remove(target_image)
     prev_img = os.path.join(IMG_DIR, name + '.png')
-    print('Copy the image into build dir ...')
     shutil.copy(prev_img, target_image)
-    print('Updating meta file ...')
     update_meta(name, TEMPLATE_DIR)
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
-    resourcepack = os.path.join(target_dir, f'totem_model_for_{name}')
-    print('Zipping ...')
+    resourcepack = os.path.join(target_dir, f'Totem_for_{name}')
+    print('打包中...')
     shutil.make_archive(resourcepack, 'zip', TEMPLATE_DIR)
-    print('ALL DONE!')
+    print('完成!')
 
 
 def full_generate(name: str):
-    save_img(name)
-    pack(name)
+    name = name.strip()
+    name_pattern = re.compile(r'^(\d|[a-zA-Z])(\d|[a-zA-Z]|_){3,15}$')
+    if re.fullmatch(name_pattern, name):
+        save_img(name)
+        pack(name)
+    else:
+        print(f'不合法的昵称: {name}')
+
+
+def full_generate_list(file):
+    if not os.path.exists(file):
+        raise RuntimeError('给定的文件不存在!')
+    with open(file) as f:
+        lines = f.readlines()
+        for line in lines:
+            name = line.strip().replace('\n', '')
+            full_generate(name)
 
 
 if __name__ == '__main__':
-    print('Please input the username:')
+    args = sys.argv
+    if len(args) > 1:
+        if args[1] == '-f' and len(args) >= 2:
+            full_generate_list(file=args[2])
+        exit()
+
+    print('')
+    print('请输入玩家的昵称:')
     username = input()
     full_generate(username)
     print('Bye!')
